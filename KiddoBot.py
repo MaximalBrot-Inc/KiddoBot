95f5f5e47e4838e5beeda2a974aca3febf17d80d
RonaldMcDonald <maxkoch@tsn.at>
2023-02-06 20:00:45 +0100

    a

--- KiddoBot.py
+++ KiddoBot.py
@@ -5,6 +5,7 @@ Maximlian && Phillip
 12.01.2023
 '''
 
+import VoiceChannel
 import discord
 import csv
 import message_handler
@@ -67,23 +68,24 @@ async def on_message(message):
 async def on_voice_state_update(member, before, after):
     global role
     if str(after.channel) == '➕ Erstelle Channel':
-        if member.nick == None:
-            channel= await after.channel.clone(name=f"{member.name}'s channel")
-        else:
-            channel= await after.channel.clone(name=f"{member.nick}'s channel")
+        await VoiceChannel.Voice(member, before, after)
+        #if member.nick == None:
+         #   channel= await after.channel.clone(name=f"{member.name}'s channel")
+        #else:
+         #   channel= await after.channel.clone(name=f"{member.nick}'s channel")
 
-        await member.move_to(channel)
-        if len(after.channel.members) == 1:
-            role = await after.channel.guild.create_role(name=f'{after.channel.name} owner' ,  color = discord.Color(633573) ,  reason = 'Channel owner')
-            await after.channel.set_permissions(role , overwrite=discord.PermissionOverwrite(manage_channels=True, connect=True, speak=True, stream=True, view_channel=True))
-            await member.add_roles(role)
+       # await member.move_to(channel)
+        #if len(after.channel.members) == 1:
+         #   role = await after.channel.guild.create_role(name=f'{after.channel.name} owner' ,  color = discord.Color(633573) ,  reason = 'Channel owner')
+          #  await after.channel.set_permissions(role , overwrite=discord.PermissionOverwrite(manage_channels=True, connect=True, speak=True, stream=True, view_channel=True))
+           # await member.add_roles(role)
 
-    if str(before.channel) != '➕ Erstelle Channel' and str(before.channel) != 'None' and str(before.channel) != '🎵 Musik':
-        if before.channel.category.name == '⋙ 🎤 Voice Channels ⋘' or '🎤 Voice Channels':
-            if len(before.channel.members) == 0:
-                await member.remove_roles(role)
-                await before.channel.delete()
-                await role.delete()
+    #if str(before.channel) != '➕ Erstelle Channel' and str(before.channel) != 'None' and str(before.channel) != '🎵 Musik':
+     #   if before.channel.category.name == '⋙ 🎤 Voice Channels ⋘' or '🎤 Voice Channels':
+      #      if len(before.channel.members) == 0:
+       #         await member.remove_roles(role)
+        #        await before.channel.delete()
+         #       await role.delete()
 
 ##############################################################################################
 
